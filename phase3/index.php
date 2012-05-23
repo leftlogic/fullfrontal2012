f9c6039c6501ab21ab20192edb6b02d54ff4ef28
Jon Linklater-Johnson <jon@binarytal.es>
2012-05-23 18:27:21 +0100

    start of phase 3

---
+++ phase3/index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html> 
+<html class="noJS">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Full Frontal 2012 - JavaScript Conference</title>
+  
+  <link rel="stylesheet" href="fullfrontal.css">
+  <link rel="shortcut icon" href="/favicon.ico">
+  <!--[if lt IE 9]>
+    <script>
+      (function(g,b){function k(){var a=e.elements;return"string"==typeof a?a.split(" "):a}function l(a){var c={},f=a.createElement,b=a.createDocumentFragment,d=b();a.createElement=function(a){if(!e.shivMethods)return f(a);var b;b=c[a]?c[a].cloneNode():m.test(a)?(c[a]=f(a)).cloneNode():f(a);return b.canHaveChildren&&!n.test(a)?d.appendChild(b):b};a.createDocumentFragment=Function("h,f","return function(){var n=f.cloneNode(),c=n.createElement;h.shivMethods&&("+k().join().replace(/\w+/g,function(a){f(a);
+      d.createElement(a);return'c("'+a+'")'})+");return n}")(e,d)}function h(a){var c;if(a.documentShived)return a;if(e.shivCSS&&!i){c=a.createElement("p");var b=a.getElementsByTagName("head")[0]||a.documentElement;c.innerHTML="x<style>article,aside,figcaption,figure,footer,header,hgroup,nav,section{display:block}mark{background:#FF0;color:#000}</style>";c=!!b.insertBefore(c.lastChild,b.firstChild)}j||(c=!l(a));if(c)a.documentShived=c;return a}var d=g.html5||{},n=/^<|^(?:button|form|map|select|textarea|object|iframe|option|optgroup)$/i,
+      m=/^<|^(?:a|b|button|code|div|fieldset|form|h1|h2|h3|h4|h5|h6|i|iframe|img|input|label|li|link|ol|option|p|param|q|script|select|span|strong|style|table|tbody|td|textarea|tfoot|th|thead|tr|ul)$/i,i,j;(function(){var a=b.createElement("a");a.innerHTML="<xyz></xyz>";i="hidden"in a;if(!(a=1==a.childNodes.length))a:{try{b.createElement("a")}catch(c){a=!0;break a}a=b.createDocumentFragment();a="undefined"==typeof a.cloneNode||"undefined"==typeof a.createDocumentFragment||"undefined"==typeof a.createElement}j=
+      a})();var e={elements:d.elements||"abbr article aside audio bdi canvas data datalist details figcaption figure footer header hgroup mark meter nav output progress section summary time video",shivCSS:!1!==d.shivCSS,shivMethods:!1!==d.shivMethods,type:"default",shivDocument:h};g.html5=e;h(b)})(this,document);
+  </script>
+  <![endif]-->
+  
+</head>
+
+<!--[if IE 8]><body class="ie"><![endif]-->
+<!--[if gt IE 8]><body><![endif]-->
+<!--[if !IE]><!--><body><!--<![endif]-->
+
+  <header>
+    <div class="ponch"></div><div class="chips"></div><!-- know your TV cop characters -->
+    <div class="wrapper">
+
+      <img class="logo" src="/images/logo.png" width="140" height="130" alt="Full Frontal">
+      
+      <time datetime="2012-11-09T09:00">09/11/12</time>
+      <h1>Full Frontal 2012</h1>
+
+    </div>
+  </header>
+
+  <div class="main"><div class="wrapper">
+    
+    <p class="lede vevent">
+      <strong class="summary">Full Frontal 2012</strong> is a one day <strong>JavaScript Conference</strong> 
+      <br>at the <span class="location">Duke of York&rsquo;s Picturehouse in Brighton, UK </span>
+      <br>on the <time class="dtstart" title="2012-11-09T09:00" datetime="2012-11-09T09:00">
+        <strong>9th November 2012</strong>
+      </time>
+    </p>
+
+    <section>
+      <h2>Speakers</h2>
+
+      <section class="speaker">
+        <h3>Full Frontal <a href="#" class="twitter">@fullfrontalconf</a></h3>
+        <img src="/images/speakers/speaker.png">
+      </section>
+
+    </section>
+
+    <section>
+      <h2>Schedule</h2>
+    </section>
+
+    <section>
+      <h2>Workshop</h2>
+    </section>
+
+    <section>
+      <h2>Locations</h2>
+      <div id="map"></div>
+    </section>
+
+    <section>
+      <h2>Sponsors</h2>
+    </section>
+
+  </div></div>
+
+  <footer>
+    <div class="date-twitter">
+      <time class="date" datetime="2012-11-09T09:00"><strong>09.11.12</strong></time>
+      <a class="twitter" href="https://twitter.com/fullfrontalconf" title="Follow Full Frontal on Twitter">@fullfrontalconf</a>
+    </div>
+    <p class="previous">
+      Previous years<br>
+      <a href="http://2011.full-frontal.org/">2011</a>,
+      <a href="http://2010.full-frontal.org/">2010</a>,
+      <a href="http://2009.full-frontal.org/">2009</a>
+    </p>
+    <a class="left-logic" href="http://leftlogic.com">Left Logic</a>
+  </footer>
+
+  <script src="/js/jquery.js"></script>
+  <!-- script src="http://maps.googleapis.com/maps/api/js?&amp;sensor=false"></script -->
+  <!-- script src="http://maps.stamen.com/js/tile.stamen.js"></script -->
+  <script src="/js/fullfrontal.js"></script>
+  <script>
+    // // Google Analytics
+    // var _gaq = _gaq || [], d = document, n = 'className', g = 'getElementById', i = 'time';
+    // _gaq.push(['_setAccount', 'UA-1656750-25']);
+    // _gaq.push(['_trackPageview']);
+    // (function() {
+    //   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    //   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    //   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    // })();
+  </script>
+
+</body> 
+</html>
