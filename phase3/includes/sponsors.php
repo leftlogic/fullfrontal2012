3235e98384b58ec9b62687c4b17c4f68d47a4655
Remy Sharp <remy@remysharp.com>
2012-06-21 20:15:33 +0100

    classes for sponsors

--- phase3/includes/sponsors.php
+++ phase3/includes/sponsors.php
@@ -33,16 +33,16 @@
 
 
 
-  <h3>Half Monty Sponsors &mdash; <a href="#">Add Yourself</a></h3>
+  <h3>Half Monty Sponsors &mdash; <a href="/sponsorship.html">Join our sponsors</a></h3>
 
-  <a class="sponsor" href="#"><img src="/images/sponsors/google.png" alt="Google"></a>
-  <a class="sponsor" href="#"><img src="/images/sponsors/mozilla.png" alt="Mozilla Developer Network"></a>
+  <a class="sponsor half" href="#"><img src="/images/sponsors/google.png" alt="Google"></a>
+  <a class="sponsor half" href="#"><img src="/images/sponsors/mozilla.png" alt="Mozilla Developer Network"></a>
 
 
-  <h3>Small Monty Sponsors &mdash; <a href="#">Add Yourself</a></h3>
+  <h3>Small Monty Sponsors &mdash; <a href="/sponsorship.html">Join our sponsors</a></h3>
 
-  <a class="sponsor" href="#"><img src="/images/sponsors/bazaarvoice.png" alt="Bazaar Voice"></a>
-  <a class="sponsor" href="#"><img src="/images/sponsors/dharmafly.png" alt="Dharmafly"></a>
+  <a class="sponsor small" href="#"><img src="/images/sponsors/bazaarvoice.png" alt="Bazaar Voice"></a>
+  <a class="sponsor small" href="#"><img src="/images/sponsors/dharmafly.png" alt="Dharmafly"></a>
 
 
 </article>
