9ecc6b2bac4de84602958576439e6fd24954ba8d
Remy Sharp <remy@remysharp.com>
2012-06-22 00:10:14 +0100

    Workshop and pre-party venue

--- phase3/includes/locations.php
+++ phase3/includes/locations.php
@@ -14,14 +14,36 @@
       </li>
     </ul>
 
+    <h3>Workshops</h3>
+    <ul>
+      <li class="venue primary-venue" data-latlng="50.825828322287116, -0.13845831155776978">
+        <span class="address">28 Kensington Street, BN1 4AJ</span>
+        <a href="http://goo.gl/maps/GTYX"><span class="bullet">B.</span>Lighthouse</a>
+        <p class="details">
+          If you've signed up to one of our workshops, this is where they're run.
+        </p>
+      </li>
+    </ul>
+
+
+    <h3>(Unoffical) Pre Party</h3>
+    <ul>
+      <li class="venue primary-venue" data-latlng="50.8254504924097, -0.13727009296417236">
+        <span class="address">101 North Road, BN1 1YE</span>
+        <a href="http://goo.gl/maps/6FH8"><span class="bullet">C.</span>Fountain Head</a>
+        <p class="details">
+          We've reserved some tables and spots, so come down from 7pm onwards and say 'hi' to your fellow <em>Full-Frontal-goer</em>!
+        </p>
+      </li>
+    </ul>
+
     <h3>After Party</h3>
     <ul>
       <li class="venue primary-venue" data-latlng="50.82181097001707,-0.14180302619934082">
         <span class="address">Preston Circus, BN1 4BA</span>
         <a href="#"><span class="bullet">B.</span>The Loft</a>
         <p class="details">
-          Please join us at the Full Frontal After Party at The Loft from 7pm until late &mdash;
-          where XXXX and XXXX, our Full Monty sponsors have bought you a pint!
+          Please join us at the Full Frontal After Party at The Loft from 7pm until late
         </p>
       </li>
     </ul>
