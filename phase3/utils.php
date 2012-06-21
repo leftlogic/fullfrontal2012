646dfc0bcb90239691676cc4e5b7750757a43c19
Remy Sharp <remy@remysharp.com>
2012-06-21 23:49:12 +0100

    Created utils.php for reuse

---
+++ phase3/utils.php
@@ -0,0 +1,41 @@
+<?php
+define('ROOT', $_SERVER['DOCUMENT_ROOT']);
+
+require_once(ROOT . '/mustache.php');
+require_once(ROOT . '/markdown.php');
+$mustache = new Mustache;
+
+$cache = array();
+
+function convertMarkdown(&$array) {
+  // Credit: http://www.php.net/manual/en/function.array-walk.php#71901
+  // var_dump($array);
+  foreach ($array as $key => $value) {
+    if (is_array($value)) {
+      $res[$key] = convertMarkdown(&$value);
+    } else {
+      if (array_pop(explode('_', $key)) == 'md') {
+        $res[$key] = Markdown($value);
+      } else {
+        $res[$key] = $value;
+      }
+    }
+  }
+  return $res;
+}
+
+function renderTemplate($data, $template) {
+  global $mustache;
+  // TODO validate the file exists - and error handle properly
+  if (!isset($cache[$template])) {
+    $cache[$template] = file_get_contents(ROOT . $template);
+  }
+
+  if (is_string($data)) {
+    $data = convertMarkdown(json_decode(file_get_contents(ROOT . $data), true));
+  }
+
+  $render = $mustache->render($cache[$template], $data);
+  return $render;
+}
+?>
