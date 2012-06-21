9544762143afe9124f9ba07d6a8bda916edbab53
Jon Linklater-Johnson <jon@binarytal.es>
2012-06-21 17:25:23 +0100

    MOAR

--- phase3/includes/sponsors.php
+++ phase3/includes/sponsors.php
@@ -3,6 +3,23 @@
 
   <h3>Full Monty Sponsors</h3>
 
+  <article class="full-monty">
+    <a class="sponsor" href="#"><img src="/images/sponsors/google.png" alt="Google"></a>
+    <div class="blurb">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris.
+      </p>
+      <p>
+        Mnisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    </div>
+  </article>
+
   <h3>Half Monty Sponsors &mdash; <a href="#">Add Yourself</a></h3>
 
   <a class="sponsor" href="#"><img src="/images/sponsors/google.png" alt="Google"></a>
