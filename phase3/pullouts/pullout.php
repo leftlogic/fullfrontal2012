27e8563f7407b235d345b1f5bf83ac947516eba9
Remy Sharp <remy@remysharp.com>
2012-06-23 00:29:45 +0100

    Analyics in

--- phase3/pullouts/pullout.php
+++ phase3/pullouts/pullout.php
@@ -29,20 +29,19 @@
   $id = $_GET['id']; // id=html5
   $type = $_GET['type']; // type=workshop
 
-  // TODO validate the file exists - and error handle properly
   echo renderTemplate('/data/' . $type . 's/' . $id . '.json', '/pullouts/' . $type . '.tmpl');
 ?>
 
   <script>
-    // // Google Analytics
-    // var _gaq = _gaq || [], d = document, n = 'className', g = 'getElementById', i = 'time';
-    // _gaq.push(['_setAccount', 'UA-1656750-25']);
-    // _gaq.push(['_trackPageview']);
-    // (function() {
-    //   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    //   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    //   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    // })();
+    // Google Analytics
+    var _gaq = _gaq || [], d = document, n = 'className', g = 'getElementById', i = 'time';
+    _gaq.push(['_setAccount', 'UA-1656750-25']);
+    _gaq.push(['_trackPageview']);
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
   </script>
 
 </body> 
