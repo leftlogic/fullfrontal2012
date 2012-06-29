5e1c20c0633afec4c28225e2b7965b1dc5753a79
Remy Sharp <remy@remysharp.com>
2012-06-29 15:09:21 +0100

    updated tracking code

--- phase3/index.php
+++ phase3/index.php
@@ -177,17 +177,19 @@ Cheers,
   <script src="/js/fullfrontal.js"></script>
   <script src="http://maps.googleapis.com/maps/api/js?&amp;sensor=false"></script>
   <script src="http://maps.stamen.com/js/tile.stamen.js"></script>
-  <script>
-    // Google Analytics
-    var _gaq = _gaq || [], d = document, n = 'className', g = 'getElementById', i = 'time';
-    _gaq.push(['_setAccount', 'UA-1656750-25']);
-    _gaq.push(['_trackPageview']);
-    (function() {
-      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
-  </script>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-1656750-30']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 <script>
 setTimeout(function () {
   var pageYOffset = window.pageYOffset === undefined ? document.body.scrollTop : window.pageYOffset;
